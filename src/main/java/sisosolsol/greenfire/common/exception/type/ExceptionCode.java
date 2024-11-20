b6e2200ad46de2c596463067adc6cad0e6ff6012
1sol2 <lhs1119r@gmail.com>
2024-11-20 16:39:20 +0900

    feat: 예외 코드 enum 추가

--- src/main/java/sisosolsol/greenfire/common/exception/type/ExceptionCode.java
+++ src/main/java/sisosolsol/greenfire/common/exception/type/ExceptionCode.java
@@ -7,21 +7,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionCode {
 
-    /*
-     * 401: Unauthorized
-     * 403: Forbidden
-     * 404: Not Found
-     * 405: Method Not Allowed
-     * 409: Conflict
-     */
-
     // 401 Error
     UNAUTHORIZED(401, "인증 되지 않은 요청입니다."),
 
     // 403 Error
     ACCESS_DENIED(403, "허가 되지 않은 요청입니다."),
 
-    InvalidForeignKeyException(1100, "외래 키 제약을 위반한 요청입니다.");;
+    InvalidForeignKeyException(1100, "외래 키 제약을 위반한 요청입니다."),
+
+    UserNotFoundException(1201, "사용자를 찾을 수 없습니다."),
+
+    DatabaseAccessException(1301, "데이터베이스 접근 중 오류가 발생했습니다.");
 
     private final int code;
     private final String message;
