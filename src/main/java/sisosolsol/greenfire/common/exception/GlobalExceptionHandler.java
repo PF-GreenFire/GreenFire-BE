5399c748d1b59206abd7b72e0df58dde519c0989
DTRT91 <kimjahen@naver.com>
2026-02-05 15:19:14 +0900

    feat: BadCredentialsException 글로벌 핸들러 추가

--- src/main/java/sisosolsol/greenfire/common/exception/GlobalExceptionHandler.java
+++ src/main/java/sisosolsol/greenfire/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,23 @@
 package sisosolsol.greenfire.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sisosolsol.greenfire.common.exception.dto.ExceptionResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> badCredentialsException(BadCredentialsException e) {
+        log.warn("인증 실패: {}", e.getMessage());
+        final ExceptionResponse exceptionResponse = ExceptionResponse.of(401, e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exceptionResponse);
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> notFoundException(NotFoundException e) {
