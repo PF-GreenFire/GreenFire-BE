25642b487d480b79902bdf4a60ecc021c02f5750
ssiion <gtldhs120@gmail.com>
2026-03-13 18:13:15 +0900

    feat: challenge update and delete

--- src/main/java/sisosolsol/greenfire/challenge/model/dao/ChallengeMapper.java
+++ src/main/java/sisosolsol/greenfire/challenge/model/dao/ChallengeMapper.java
@@ -1,10 +1,13 @@
 package sisosolsol.greenfire.challenge.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import sisosolsol.greenfire.challenge.model.dto.ChallengeCreateDTO;
 import sisosolsol.greenfire.challenge.model.dto.ChallengeDTO;
 import sisosolsol.greenfire.challenge.model.dto.ChallengePartDTO;
 import sisosolsol.greenfire.challenge.model.dto.ChallengeSearchCondition;
+import sisosolsol.greenfire.challenge.model.dto.ChallengeUpdateDTO;
+import sisosolsol.greenfire.common.enums.challenge.ChallengeStatus;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +22,12 @@ public interface ChallengeMapper {
     List<ChallengeDTO> selectChallenges(ChallengeSearchCondition condition);
 
     ChallengeDTO selectChallengeByCode(Integer challengeCode);
+
+    int updateChallenge(@Param("challengeCode") Integer challengeCode,
+                        @Param("update") ChallengeUpdateDTO update);
+
+    int updateChallengeStatus(@Param("challengeCode") Integer challengeCode,
+                              @Param("status") ChallengeStatus status);
 
     int countCurrentParticipants(Integer challengeCode);
 
